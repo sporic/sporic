9aba3daac9edcf9f9d98eacae60f130e08d3c86b
Amal <amalb11164@gmail.com>
2024-09-03 23:02:54 +0530

    base work

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
-	"strconv"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
@@ -16,7 +17,10 @@ import (
 var session = scs.New()
 
 type App struct {
-	Db *sql.DB
+	infoLog       *log.Logger
+	erroLog       *log.Logger
+	Db            *sql.DB
+	templateCache map[string]*template.Template
 }
 
 func init() {
@@ -30,18 +34,30 @@ func init() {
 }
 
 func main() {
-	host := flag.String("host", "", "address to host the site")
-	port := flag.Int("port", 8080, "port to host the site")
+	addr := flag.String("addr", ":8080", "address to host the site")
 	flag.Parse()
 
-	app := App{}
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	app := App{infoLog: infoLog, erroLog: errorLog, templateCache: templateCache}
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", app.signin)
 
-	err := http.ListenAndServe(*host+":"+strconv.Itoa(*port), session.LoadAndSave(mux))
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
 	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
