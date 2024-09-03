9d2b493ff36d8fa0079c693847717c38a2c2dd8a
heet-10104 <heet.bjhaveri2022@vitstudent.ac.in>
2024-09-03 18:49:27 +0530

    sign in progress

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// var templates = template.Must(template.ParseGlob("template/*.html"))
+var session = scs.New()
+
+func init() {
+	db, err := sql.Open("mysql", "newuser:newpassword@/sporic")
+	if err != nil {
+		panic(err)
+	}
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+}
+
+func main() {
+	host := flag.String("host", "", "address to host the site")
+	port := flag.Int("port", 8080, "port to host the site")
+	flag.Parse()
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", signin)
+	
+
+	err := http.ListenAndServe(*host+":"+strconv.Itoa(*port), session.LoadAndSave(mux))
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module sporic
+
+go 1.22.1
+
+require (
+	github.com/alexedwards/scs/v2 v2.8.0
+	github.com/mattn/go-sqlite3 v1.14.22
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+)
