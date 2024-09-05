a55dca6d2cdf04e191de51b8a02070bf1c7ab506
Amal <amalb11164@gmail.com>
2024-09-05 10:12:16 +0530

    make invalid credentials error

--- internal/models/users.go
+++ internal/models/users.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"database/sql"
-	"fmt"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
@@ -26,14 +25,14 @@ func (m *UserModel) Authenticate(username string, password string) (int, error) 
 	var hashedPassword []byte
 	err := m.Db.QueryRow("select id, hashed_password from users where username = ?", username).Scan(&id, &hashedPassword)
 	if err == sql.ErrNoRows {
-		fmt.Println("no user exists")
-	} else {
-		fmt.Println(err)
+		return 0, ErrInvalidCredentials
+	} else if err != nil {
+		return 0, err
 	}
 
 	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
 	if err != nil {
-		return 0, err
+		return 0, ErrInvalidCredentials
 	}
 	return id, nil
 }
