64d68d7969574e03e3fdd5fcb48d5fcb1e38c1d5
heet-10104 <heet.bjhaveri2022@vitstudent.ac.in>
2024-09-11 18:22:46 +0530

    add application made and database modified

--- internal/models/applications.go
+++ internal/models/applications.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 )
 
 type Application struct {
@@ -65,4 +67,22 @@ func (m *ApplicationModel) Fetch(sporic_ref_no string, leader string) []Applicat
 
 func (m *ApplicationModel) Insert(form Application) {
 
+	var count int
+
+	row, err := m.Db.Query("select count(*) from applications where FinancialYear= ?", form.FinancialYear)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = row.Scan(&count)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sporic_ref_no := "cc" + strings.ToLower(form.ActivityType) + form.FinancialYear + strconv.Itoa(count)
+
+	_, err = m.Db.Exec("insert into applications (sporic_ref_no, financial_year, activity_type, leader, estimated_amt, company_name, company_adress, contact_person, mail_id, mobile, gst, pan_number, status) values (?,?,?,?,?,?,?,?,?,?,?,?,?)", sporic_ref_no, form.FinancialYear, form.FinancialYear, form.ActivityType, "dummy", form.EstimatedAmt, form.CompanyName, form.CompanyAddress, form.ContactPerson, form.MailID, form.Mobile, form.GST, form.PanNumber, "pending")
+
+	if err !=nil{
+		log.Fatal(err)
+	}
 }
